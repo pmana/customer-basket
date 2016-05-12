8973c0ba7780aa1e21ef3616e9c95efb19f7cc1a
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 17:36:25 +0100

    Start implementing basket with test for one product total

---
+++ CustomerBasket.Tests/BasketTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+
+namespace CustomerBasket.Tests
+{
+    [TestFixture]
+    public class BasketTests
+    {
+        private Basket basket;
+
+        [SetUp]
+        public void SetUp()
+        {
+            basket = new Basket();
+        }
+
+        [Test]
+        public void CalculateTotal_ForOneProduct_ReturnsThatProductsValue()
+        {
+            basket.AddProducts(new Bread());
+
+            var total = basket.CalculateTotal();
+
+            Assert.That(total, Is.EqualTo(1));
+        }
+    }
+}

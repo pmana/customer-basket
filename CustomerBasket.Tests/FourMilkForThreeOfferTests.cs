38119e68231a0a0d295de2e839d84c5761abac70
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 22:03:20 +0100

    Add additional tests for 4 for 3 milk offer to ensure it works for more multiple of 4 and isn't affected by other products

--- CustomerBasket.Tests/FourMilkForThreeOfferTests.cs
+++ CustomerBasket.Tests/FourMilkForThreeOfferTests.cs
@@ -65,5 +65,52 @@ namespace CustomerBasket.Tests
             Assert.That(discounts[0].Value, Is.EqualTo(1.15m));
             Assert.That(discounts[1].Value, Is.EqualTo(1.15m));
         }
+
+        [Test]
+        public void CalculateDiscounts_ForElevenMilk_ReturnsTwoDiscountsForThePriceOfOneMilk()
+        {
+            var discounts = offer.CalculateDiscounts(
+                new[] { Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk,
+                    Product.Milk, Product.Milk, Product.Milk, Product.Milk }).ToArray();
+
+            Assert.That(discounts, Has.Length.EqualTo(2));
+            Assert.That(discounts[0].Value, Is.EqualTo(1.15m));
+            Assert.That(discounts[1].Value, Is.EqualTo(1.15m));
+        }
+
+        [Test]
+        public void CalculateDiscounts_ForTwelveMilk_ReturnsTwoDiscountsForThePriceOfOneMilk()
+        {
+            var discounts = offer.CalculateDiscounts(
+                new[] { Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk,
+                    Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk }).ToArray();
+
+            Assert.That(discounts, Has.Length.EqualTo(3));
+            Assert.That(discounts[0].Value, Is.EqualTo(1.15m));
+            Assert.That(discounts[1].Value, Is.EqualTo(1.15m));
+            Assert.That(discounts[2].Value, Is.EqualTo(1.15m));
+        }
+
+        [Test]
+        public void CalculateDiscounts_ForFourMilkAlongsideOtherProducts_ReturnsOneDiscountForThePriceOfOneMilk()
+        {
+            var discounts = offer.CalculateDiscounts(
+                new[] { Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Bread, Product.Butter }).ToArray();
+
+            Assert.That(discounts, Has.Length.EqualTo(1));
+            Assert.That(discounts[0].Value, Is.EqualTo(1.15m));
+        }
+
+        [Test]
+        public void CalculateDiscounts_ForEightMilkAlongsideOtherProducts_ReturnsTwoDiscountsForThePriceOfOneMilk()
+        {
+            var discounts = offer.CalculateDiscounts(
+                new[] { Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk, Product.Milk,
+                    Product.Milk, Product.Milk, Product.Bread, Product.Bread, Product.Bread, Product.Bread  }).ToArray();
+
+            Assert.That(discounts, Has.Length.EqualTo(2));
+            Assert.That(discounts[0].Value, Is.EqualTo(1.15m));
+            Assert.That(discounts[1].Value, Is.EqualTo(1.15m));
+        }
     }
 }

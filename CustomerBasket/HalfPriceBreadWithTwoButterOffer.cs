86a5b6b146aa46d5220d74f49969f0dd1c56ac6f
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 22:18:05 +0100

    Improve implementation of half price bread offer to work with multiple eligible sets of products

--- CustomerBasket/HalfPriceBreadWithTwoButterOffer.cs
+++ CustomerBasket/HalfPriceBreadWithTwoButterOffer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,16 +8,11 @@ namespace CustomerBasket
     {
         public IEnumerable<Discount> CalculateDiscounts(IEnumerable<Product> products)
         {
-            var butter = products.Count(x => x.ProductId == Product.Butter.ProductId);
-            var bread = products.Count(x => x.ProductId == Product.Bread.ProductId);
-            if (butter == 2 && bread == 1)
-            {
-                yield return new Discount(Product.Bread, 50);
-            }
-            else
-            {
-                yield break;
-            }
+            var allProducts = products.ToArray();
+            var eligibleButter = (int) Math.Floor(allProducts.Count(x => x.ProductId == Product.Butter.ProductId)/2d);
+            var eligibleBread = allProducts.Count(x => x.ProductId == Product.Bread.ProductId);
+            return Enumerable.Range(0, Math.Min(eligibleBread, eligibleButter))
+                .Select(x => new Discount(Product.Bread, 50));
         }
     }
 }

fb5f4ff35383a46ce48e2001b0c80f835ced070a
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 17:06:55 +0100

    Add outline for BDD scenarios provided

---
+++ CustomerBasket.Tests/Scenarios/BasketScenarios.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace CustomerBasket.Tests.Scenarios
+{
+    // in a real project these would likely be broken up into their own classes per scenario, or following a
+    // framework like SpecFlow
+    [TestFixture]
+    public class BasketScenarios
+    {
+        [Test]
+        public void GivenTheBasketHasOneBreadButterAndMilk_WhenITotalTheBasket_ThenTheTotalShouldBe2_95()
+        {
+            
+        }
+
+        [Test]
+        public void GivenTheBasketHasTwoBreadAndTwoButter_WhenITotalTheBasket_ThenTheTotalShouldBe3_10()
+        {
+            
+        }
+
+        [Test]
+        public void GivenTheBasketHasFourMilk_WhenITotalTheBasket_ThenTheTotalShouldBe3_45()
+        {
+            
+        }
+
+        [Test]
+        public void GivenTheBasketHasTwoButterOneBreadAndEightMilk_WhenITotalTheBasket_ThenTheTotalShouldBe9_00()
+        {
+            
+        }
+    }
+}

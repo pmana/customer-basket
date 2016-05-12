fb18b4ebdf96aeddabaab850778f8facfd3274bc
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 21:41:09 +0100

    Add test to check that the (stub) offers repository returns some offers

---
+++ CustomerBasket.Tests/OffersRespositoryTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+
+namespace CustomerBasket.Tests
+{
+    [TestFixture]
+    public class OffersRespositoryTests
+    {
+        [Test]
+        public void GetOffers_ReturnsANonEmptySetOfOffers()
+        {
+            var repository = new OffersRepository();
+
+            var offers = repository.GetOffers();
+
+            Assert.That(offers, Is.Not.Empty);
+        }
+    }
+}

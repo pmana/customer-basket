604faa0fff7f2121d51568900fd5a6106acc2248
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 22:05:47 +0100

    Add tests for half price bread offer with test for no products

---
+++ CustomerBasket.Tests/HalfPriceBreadWithTwoButterOfferTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CustomerBasket.Tests
+{
+    [TestFixture]
+    public class HalfPriceBreadWithTwoButterOfferTests
+    {
+        private HalfPriceBreadWithTwoButterOffer offer;
+
+        [SetUp]
+        public void SetUp()
+        {
+            offer = new HalfPriceBreadWithTwoButterOffer();
+        }
+
+        [Test]
+        public void CalculateDiscounts_ForNoProducts_ReturnsNoDiscount()
+        {
+            var discounts = offer.CalculateDiscounts(Enumerable.Empty<Product>());
+
+            Assert.That(discounts, Is.Empty);
+        }
+    }
+}

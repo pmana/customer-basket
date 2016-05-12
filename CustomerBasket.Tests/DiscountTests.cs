f573376b06e5234c2fa754f38292aeb65c753101
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 19:29:36 +0100

    Add DiscountTests and new constructor for Discount, and failing test for 0% discount

---
+++ CustomerBasket.Tests/DiscountTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace CustomerBasket.Tests
+{
+    [TestFixture]
+    public class DiscountTests
+    {
+        [Test]
+        public void Discount_WhenZeroPercent_SetsValueToValueOfOriginalProduct()
+        {
+            var discount = new Discount(Product.Bread, 0);
+
+            Assert.That(discount.Value, Is.EqualTo(1));
+        }
+    }
+}
--- CustomerBasket/Discount.cs
+++ CustomerBasket/Discount.cs
@@ -2,6 +2,10 @@
 {
     public class Discount
     {
-        
+        public Discount(Product product, decimal percentage)
+        {
+        }
+
+        public decimal Value { get; private set; }
     }
 }

2f6be767f2de418e0994f60d067a5271bf364274
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 17:51:17 +0100

    Add new product class to represent all products

---
+++ CustomerBasket/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CustomerBasket
+{
+    public class Product
+    {
+        public Guid ProductId { get; private set; }
+        public decimal Value { get; private set; }
+
+        // these would normally come from a datastore, they are here as static members for convenience
+        public static readonly Product Bread = new Product
+        {
+            ProductId = Guid.NewGuid(),
+            Value = 1
+        };
+
+        public static readonly Product Butter = new Product
+        {
+            ProductId = Guid.NewGuid(),
+            Value = 0.8m
+        };
+
+        public static readonly Product Milk = new Product
+        {
+            ProductId = Guid.NewGuid(),
+            Value = 1.15m
+        };
+    }
+}

e0786fc5c8045242d14ddf8a11effe6e47f3df3b
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 21:57:27 +0100

    Implement 4 for 3 offer with basic logic for one offer's worth of milk

--- CustomerBasket/FourMilkForThreeOffer.cs
+++ CustomerBasket/FourMilkForThreeOffer.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 namespace CustomerBasket
@@ -8,6 +7,12 @@ namespace CustomerBasket
     {
         public IEnumerable<Discount> CalculateDiscounts(IEnumerable<Product> products)
         {
+            var eligibleProducts = products.Count(x => x.ProductId == Product.Milk.ProductId);
+
+            if (eligibleProducts >= 4)
+            {
+                return new[] {new Discount(Product.Milk, 100)};
+            }
             return Enumerable.Empty<Discount>();
         }
     }

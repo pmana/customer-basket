e3c5f1b011b9c75fac7fa63c3edcd2de561fbe6c
Phil Ananin <phil.ananin@gmail.com>
2016-05-12 21:39:05 +0100

    Add additional tests for multiple discounts in customer basket

--- CustomerBasket.Tests/BasketTests.cs
+++ CustomerBasket.Tests/BasketTests.cs
@@ -109,5 +109,37 @@ namespace CustomerBasket.Tests
 
             Assert.That(total, Is.EqualTo(1.3m));
         }
+
+        [Test]
+        public void CalculateTotal_AddingTwoProductsWithTwoDiscounts_ReturnsThoseProductsValuesMinusBothDiscounts()
+        {
+            basket.AddProducts(Product.Bread, Product.Butter);
+            var offer = MockRepository.GenerateMock<IOffer>();
+            var discount1 = new Discount(Product.Bread, 50);
+            var discount2 = new Discount(Product.Butter, 10);
+            offer.Stub(x => x.CalculateDiscount(null)).IgnoreArguments().Return(new[] {discount1, discount2});
+            offersRepository.Stub(x => x.GetOffers()).Return(new[] {offer});
+
+            var total = basket.CalculateTotal();
+
+            Assert.That(total, Is.EqualTo(1.22m));
+        }
+
+        [Test]
+        public void CalculateTotal_AddingThreeProductsWithTwoDiscountsInTwoOffers_ReturnsThoseProductsValuesMinusBothDiscounts()
+        {
+            basket.AddProducts(Product.Bread, Product.Butter, Product.Milk);
+            var offer1 = MockRepository.GenerateMock<IOffer>();
+            var discount1 = new Discount(Product.Bread, 50);
+            offer1.Stub(x => x.CalculateDiscount(null)).IgnoreArguments().Return(new[] {discount1});
+            var offer2 = MockRepository.GenerateMock<IOffer>();
+            var discount2 = new Discount(Product.Butter, 20);
+            offer2.Stub(x => x.CalculateDiscount(null)).IgnoreArguments().Return(new[] {discount2});
+            offersRepository.Stub(x => x.GetOffers()).Return(new[] { offer1, offer2 });
+
+            var total = basket.CalculateTotal();
+
+            Assert.That(total, Is.EqualTo(2.29m));
+        }
     }
 }
